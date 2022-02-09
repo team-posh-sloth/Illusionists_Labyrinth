cbddeb25e3bc810e7677057eec1065767e82fed5
SpellMender <spellmender@gmail.com>
2022-02-08 19:32:18 -0700

    Gravity is fixed (for real this time)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,10 +2,10 @@ using System.Collections; using System.Collections.Generic; using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed, rotSpeed, gravity = 9.8f;
+    [SerializeField] float moveSpeed, rotSpeed, gravity = 9.8f, normalForce = 1f;
 
     float rotation, gravVelocity;
-
+    
     CharacterController character;
 
     void Start()
@@ -16,15 +16,24 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        UpdateRotation(); // Moving the mouse rotates the player left and right
-        UpdateXZ(); // Planar input is added to player movement
+        UpdateRotation(); // Moving the mouse rotates the player on the Y axis
+        UpdateXZMovement(); // Planar input is added to player movement
         UpdateGravity(); // Gravity is added to player movement
     }
 
     // Mouse input on the X axis increases the object's rotation on its Y axis (rotSpeed is the maximum rotation in degrees per frame)
     void UpdateRotation() { rotation += Input.GetAxis("Mouse X") * rotSpeed; transform.eulerAngles = new Vector3(0, rotation); }
-    // Planar input (i.e. WASD) creates a normalized vector which is then multiplied to the magnitude of units (moveSpeed) per second (Time.deltaTime) of character movement
-    void UpdateXZ() { character.Move(moveSpeed * Time.deltaTime * (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized); }
-    void UpdateGravity() { if (character.isGrounded) { gravVelocity = 0; } else { gravVelocity += gravity * Time.deltaTime; } character.Move(-gravVelocity * Time.deltaTime * transform.up); }
+
+    // XZ axis input (i.e. WASD) creates a vector on the XZ plane (forward and right) with a magnitude of units (moveSpeed) per second (deltaTime)
+    void UpdateXZMovement()  { 
+        character.Move(moveSpeed * Time.deltaTime * (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized);
+    }
+
+    // The character moves downward (negative up) at velocity (gravVelocity) per second (deltaTime)
+    void UpdateGravity() {
+        character.Move(gravVelocity * Time.deltaTime * -transform.up);  // Move() must be called before isGrounded              <--|| required for proper   ||
+        if (character.isGrounded) { gravVelocity = normalForce; }       // A static normal force must be applied when grounded  <--|| ground detection      ||
+        else { gravVelocity += gravity * Time.deltaTime; } // increase gravVelocity by meters (gravity) per second (deltaTime)
+    }
 
 }
