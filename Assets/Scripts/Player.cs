6b19ddc933332efef80f76418269c7ddb9e41546
SpellMender <spellmender@gmail.com>
2022-02-15 13:45:07 -0700

    Animations added but not mapped

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed, rotSpeed, gravity = 9.8f, normalForce = 1f;
-    [SerializeField] [Range(0, 4)] [Tooltip("")] int zoomSpeed;
+    [SerializeField] [Tooltip("Velocity per second")] float gravity = 9.8f, normalForce = 1f;
+    [SerializeField] [Tooltip("Meters per second")] float moveSpeed;
+    [SerializeField] [Range(0, 360)] [Tooltip("Max input degrees per second")] float rotSpeed;
+    [SerializeField] [Range(0, 5)] [Tooltip("Max input zoom percentage per second")] float zoomSpeed;
 
-    float yRot, gravVelocity;
-
-    float yPos, zPos, xRot, zoomPercentage;
+    float xRot, yRot, yPos, zPos, gravVelocity, zoomPercentage, runMultiplier;
 
     Transform cam;
     
@@ -17,31 +17,32 @@ public class Player : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked; // Locks the cursor to Game Window (Esc. key frees it in editor)
         character = GetComponent<CharacterController>(); // References the Character Controller
-        cam = Camera.main.transform;
+        cam = Camera.main.transform; // References the main camera transform
     }
 
     void Update()
     {
-        UpdateRotation(); // Moving the mouse rotates the player on the Y axis
+        UpdateRotation(); // Moving the mouse on the X axis rotates the player on the Y axis
+        UpdateZoom(); // Moving the mouse on the Y axis controls camera zoom on the player
         UpdateXZMovement(); // Planar input is added to player movement
         UpdateGravity(); // Gravity is added to player movement
     }
 
-    // Mouse input on the X axis increases the object's rotation on its Y axis (rotSpeed is the maximum rotation in degrees per frame)
-    void UpdateRotation()
-    {
-        // Lateral mouse movement
-        yRot += Input.GetAxis("Mouse X") * rotSpeed; transform.eulerAngles = new Vector3(0, yRot);
+    // Mouse input on the X axis increases the object's rotation on its Y axis (rotSpeed is the maximum rotation in degrees per second)
+    void UpdateRotation() { yRot += Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed; transform.eulerAngles = new Vector3(0, yRot); }
 
-        // Camera zoom
-        zoomPercentage += Input.GetAxis("Mouse Y") * Time.deltaTime * zoomSpeed; zoomPercentage = Mathf.Clamp01(zoomPercentage);
-        yPos = Mathf.Lerp(4, 2, zoomPercentage); zPos = Mathf.Lerp(-7.5f, -3, zoomPercentage); xRot = Mathf.Lerp(30, 15, zoomPercentage);
-        Camera.main.transform.localPosition = new Vector3(0, yPos, zPos); cam.localEulerAngles = new Vector3(xRot, 0);
+    // Mouse input on the Y axis interpolates both the position and angle of the camera behind the player (zoomSpeed is zoom percentage per second)
+    void UpdateZoom() 
+    {
+        zoomPercentage += Input.GetAxis("Mouse Y") * Time.deltaTime * zoomSpeed; zoomPercentage = Mathf.Clamp01(zoomPercentage); // zoomPercentage is clamped between 0 and 1 (1 is 100%)
+        yPos = Mathf.Lerp(4, 2, zoomPercentage); zPos = Mathf.Lerp(-7.5f, -3, zoomPercentage); xRot = Mathf.Lerp(30, 15, zoomPercentage); // yPos, xPos and xRot interpolate at the rate of zoomPercentage)
+        cam.localPosition = new Vector3(0, yPos, zPos); cam.localEulerAngles = new Vector3(xRot, 0); // localPosition must be used to keep values relative to player
     }
 
     // XZ axis input (i.e. WASD) creates a vector on the XZ plane (forward and right) with a magnitude of units (moveSpeed) per second (deltaTime)
-    void UpdateXZMovement()  { 
-        character.Move(moveSpeed * Time.deltaTime * (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized);
+    void UpdateXZMovement()  {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W)) runMultiplier = 2; else runMultiplier = 1; // Player moves twice as fast holding shift
+        character.Move(moveSpeed * runMultiplier * Time.deltaTime * (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized);
     }
 
     // The character moves downward (negative up) at velocity (gravVelocity) per second (deltaTime)
