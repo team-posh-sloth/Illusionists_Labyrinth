82fa6ec2d1df855fcf92a6289f11b8ec074f5b67
Ammron Phillips <33878790+5hadowduck@users.noreply.github.com>
2022-03-29 23:01:33 -0600

    Blendtree coding + ooopssssieees

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -10,6 +10,9 @@ public class CharacterAnimation : MonoBehaviour
     float velocityX = 0.0f;
     public float acceleration = 2.0f;
     public float deceleration = 2.0f;
+    public float maxWalkVelocity = 1.0f;
+    public float maxRunVelocity = 2.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,22 +29,26 @@ public class CharacterAnimation : MonoBehaviour
         bool backPressed = Input.GetKey("s");
         bool runPressed = Input.GetKey("left shift");
 
+        //set current max velocity
+        float currentMaxVelocity = runPressed ? maxRunVelocity : maxWalkVelocity;
 
-        if (forwardPressed && velocityX < 1.0f && !runPressed)
+        //button presses
+        if (forwardPressed && velocityX < currentMaxVelocity)
         {
             velocityZ += Time.deltaTime * acceleration;
         }
 
-        if (rightPressed && velocityZ > -1.0f && !runPressed)
+        if (rightPressed && velocityZ > -currentMaxVelocity)
         {
             velocityX -= Time.deltaTime * acceleration;
         }
 
-        if (leftPressed && velocityZ < 1.0f && !runPressed)
+        if (leftPressed && velocityZ < currentMaxVelocity)
         {
             velocityX += Time.deltaTime * acceleration;
         }
 
+        //decelerations
         if (!forwardPressed && velocityZ > 0.0f)
         {
             velocityZ -= Time.deltaTime * deceleration;
@@ -52,6 +59,34 @@ public class CharacterAnimation : MonoBehaviour
             velocityZ = 0.0f;
         }
 
+        if (!rightPressed && velocityX < 0.0f)
+        {
+            velocityX += Time.deltaTime * deceleration;
+        }
+       
+        if (!leftPressed && velocityX > 0.0f)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+        }
+        //if no buttons pressed and velocity isn't zero set to zero
+        if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
+        {
+            velocityX = 0.0f;
+        }
+
+        if (forwardPressed && runPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX = currentMaxVelocity;
+        }
+        else if (forwardPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+        }
+        else if (forwardPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity - 1.0f))
+        {
+            velocityX = currentMaxVelocity;
+        }
+        //set anim floats
         animator.SetFloat("velocityZ", velocityZ);
         animator.SetFloat("velocityX", velocityX);
 
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    bool inRange;
+
+    void Update() { if (Input.GetKeyDown(KeyCode.E) && inRange) { Interact(); } } // Interact is called when pressing "E" in trigger range (GetKeyDown works most reliably in Update)
+
+    void OnTriggerEnter(Collider other) { if (other.name == "Player") { Detect(); inRange = true; } } // Detect is called when player enters trigger range
+
+    void OnTriggerStay(Collider other) { if (other.name == "Player") { Watch(); } } // Watch is called when player stays in trigger range
+
+    void OnTriggerExit(Collider other) { if (other.name == "Player") { Undetect(); inRange = false; } } // Undetect is called when player exits trigger range
+
+    protected virtual void Detect() { } // Virtual function to be customized by children (i.e. Dialogue)
+
+    protected virtual void Watch() { } // Virtual function to be customized by children
+
+    protected virtual void Interact() { } // Virtual function to be customized by children
+
+    protected virtual void Undetect() { } // Virtual function to be customized by children
+
+}
